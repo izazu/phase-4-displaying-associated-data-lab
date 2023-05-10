298d58ca3736d2bea0d114928bd0bad6f7ddc976
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2023-05-10 19:32:29 +0200

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all
+      render json: items.to_json(include: { user: { only: [:id, :username, :city] } })
+    end
+  end
+  
